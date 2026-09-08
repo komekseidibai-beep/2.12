458cb04d2481128ec63c834935e4a367a54269ab
Komek <https://github.com/komekseidibai-beep/2.4.git>
2026-09-08 08:48:05 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(child: Text("Magzhan Mall ", textDirection: TextDirection.ltr,style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 40),)),
+  );
+}
