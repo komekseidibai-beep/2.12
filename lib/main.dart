480b5b06e03f7c6264e5e54cc296b0dca5abd122
Komek <https://github.com/komekseidibai-beep/2.4.git>
2026-09-09 14:59:10 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(child: Text("Magzhan Mall ", textDirection: TextDirection.ltr,style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 40),)),
-  );
+void main() => runApp(First());
+class First extends StatefulWidget{@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return(Second());
+    
+  }}
+  class Second extends State<First>{
+    double _value = 0;
+
+void increase() {
+  
+  setState(() {
+    _value++;
+  });
+
 }
+void decrease() {
+  
+  setState(() {
+    _value--;
+  });
+
+}
+
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text("Counter"),centerTitle: true,backgroundColor: Colors.red,),
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Press"),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(onPressed: (){
+                  increase();
+                }, icon: Icon(Icons.add)),
+                Text("${_value}"),
+                IconButton(onPressed: (){decrease();
+                }, icon: Icon(Icons.remove)),
+              
+              ],
+            ),
+          )
+          
+        ],
+      ),),
+    ));
+  }
+  }
+
+  
+
+  
+
+
